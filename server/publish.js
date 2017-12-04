8a9098a04bdc9d30538a566bd18d550892bdbdc6
wangyanfsd <github@wangyanfsd.cn>
2017-12-04 10:35:32 +0800

    发布-系统管理-组织机构管理

--- server/publish.js
+++ server/publish.js
@@ -25,6 +25,10 @@ Meteor.startup(function () {
     Meteor.publish('xitongguanli_mabiaoxx', function () {
         return ts_gc_mabiaoxx.find({});
     });
+    // 发布-系统管理-组织机构管理
+    Meteor.publish('xitongguanli_zuzhuqxgl', function () {
+        return ts_gc_zuzhijg.find({});
+    });
     Meteor.publish('zijinbofu_gongdanxx', function () {
         return tb_gc_zijinbflcxx.find({}/*,{fields:{
             'shenpidbh':true,
