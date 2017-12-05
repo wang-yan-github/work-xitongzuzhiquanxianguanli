74201f5080546f8f8ae2d5a5abee9ddbd7d87014
wangyanfsd <github@wangyanfsd.cn>
2017-12-05 20:13:30 +0800

    系统管理-组织机构管理-js-pdate/insert文档(部分完成)-优化响应式(部分完成)-清空输入框(部分完成)

--- client/views/pages/xitonggl/zuzhiqxgl.js
+++ client/views/pages/xitonggl/zuzhiqxgl.js
@@ -1,5 +1,3 @@
-var zuzhiqxglxxLength = 0;
-var zuzhiqxglxx;
 Template.zuzhiqxgl.onCreated(function () {
     // 原始-组织权限管理
     this.yuanshi_zuzhiqxglxx = new ReactiveVar(0);
@@ -8,9 +6,11 @@ Template.zuzhiqxgl.onCreated(function () {
 
     handle = this.subscribe("xitongguanli_zuzhuqxgl");
     /*订阅并获取数据*/
-    Tracker.autorun(function () {
-        Template.instance().yuanshi_zuzhiqxglxx.set(ts_gc_zuzhijg.find({}));
-        Template.instance().zuzhiqxglxx.set(Template.instance().yuanshi_zuzhiqxglxx.get());
+    Tracker.autorun(function (computation) {
+        if (handle.ready()){
+            Session.set('zuzhiqxglxx',ts_gc_zuzhijg.find({}).fetch());
+            computation.stop();
+        }
     });
 
 
@@ -18,8 +18,89 @@ Template.zuzhiqxgl.onCreated(function () {
 
 Template.zuzhiqxgl.onRendered(function () {
     Tracker.autorun(function () {
-        zuzhiqxglxx = ts_gc_zuzhijg.find({}).fetch();
+        //zuzhiqxglxx = ts_gc_zuzhijg.find({}).fetch();
+        var zuzhiqxglxx = Session.get('zuzhiqxglxx');
         if (handle.ready()){
+            // 重新生成
+            //$('#plugins1').jstree(true).refresh();
+            // 销毁
+            $('#plugins1').jstree("destroy");
+
+            var fanhui_zuzhiqxglxx = new Array();
+            // 组织结构信息
+            for(var i in zuzhiqxglxx){
+                var obj = {};
+
+                obj.id = zuzhiqxglxx[i].jigoubh + 'zuzhiqxglxx' + [i];
+                obj.text = zuzhiqxglxx[i].jigoumc;
+                obj.parent = '#';
+                fanhui_zuzhiqxglxx.push(obj);
+                // 部门信息
+                for(var j in zuzhiqxglxx[i].bumenxx){
+                    var obj = {};
+                    obj.id = zuzhiqxglxx[i].bumenxx[j].bumenbh + 'zuzhiqxglxx' + [i] + 'bumenxx' +[j];
+                    obj.text = zuzhiqxglxx[i].bumenxx[j].bumenmc;
+                    obj.parent = zuzhiqxglxx[i].jigoubh + 'zuzhiqxglxx' + [i];
+                    fanhui_zuzhiqxglxx.push(obj);
+
+                    // 人员信息
+                    for(var k in zuzhiqxglxx[i].bumenxx[j].renyuanxx){
+                        var obj = {};
+                        obj.id = zuzhiqxglxx[i].bumenxx[j].renyuanxx[k].zhanghaobh + [i] + 'bumenxx' +[j] + 'renyuanxx' + [k];
+                        obj.text = zuzhiqxglxx[i].bumenxx[j].renyuanxx[k].xingming;
+                        obj.parent = zuzhiqxglxx[i].bumenxx[j].bumenbh + 'zuzhiqxglxx' + [i] + 'bumenxx' +[j];
+                        fanhui_zuzhiqxglxx.push(obj);
+                    }
+                }
+            }
+
+            var demo = [
+                { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" },
+                { "id" : "ajson2", "parent" : "#", "text" : "Root node 2" },
+                { "id" : "ajson3", "parent" : "ajson2", "text" : "Child 1" },
+                { "id" : "ajson4", "parent" : "ajson2", "text" : "Child 2" },
+            ];
+
+            //console.log(fanhui_zuzhiqxglxx);
+            //console.log(demo);
+
+
+
+            $("#plugins1").jstree({
+                "checkbox" : {
+                    "keep_selected_style" : false,
+                    "three_state": false
+                },
+                "types" : {
+                    'default' : {
+                        'icon' : 'fa fa-folder'
+                    },
+                },
+                /*"contextmenu": {
+                    "items": function ($node) {
+                        var tree = $("#tree").jstree(true);
+                        return {
+                            "Create": {
+                                "separator_before": false,
+                                "separator_after": false,
+                                "label": "新增",
+                            },
+                            "Edit": {
+                                "separator_before": false,
+                                "separator_after": false,
+                                "label": "编辑",
+                            },
+                        };
+                    }
+                },*/
+
+                /*"plugins" : [ "checkbox","types", "contextmenu" ],*/
+                'plugins' : [ 'types', 'dnd' ],
+                'core' : {
+                    'data': fanhui_zuzhiqxglxx
+                }
+            });
+
             Tracker.afterFlush(function () {
 
                 /*添加部门列表数据*/
@@ -36,8 +117,6 @@ Template.zuzhiqxgl.onRendered(function () {
                 // 获取机构id,作为唯一键.
                 // 获取机构名称显示
                 // 获取部门列表显示
-
-
                 for(var i in zuzhiqxglxx){
                     // 新增人员
                     $("#xinzengryjg").append("<option value='"+zuzhiqxglxx[i]._id+"'>"+zuzhiqxglxx[i].jigoumc+"</option>");
@@ -69,91 +148,6 @@ Template.zuzhiqxgl.onRendered(function () {
                     }
                 }
 
-                /*// 二级联动 根据机构获取部门列表信息
-                $("#xinzengryjg").change(function(){
-                    for(var i =0; i < zuzhiqxglxx.length; i ++){
-                        $('.zuzhiqxglxx'+index).hide();
-                    }
-                    var index = $(this).get(0).selectedIndex;
-                    //$('.zuzhiqxglxx'+index).hide().eq(index).show();
-                    $('.zuzhiqxglxx'+index).show();
-                    console.log($(this).get(0).selectedIndex);
-                });*/
-
-
-
-                var fanhui_zuzhiqxglxx = new Array();
-                // 组织结构信息
-                for(var i in zuzhiqxglxx){
-                    var obj = {};
-
-                    obj.id = zuzhiqxglxx[i].jigoubh + 'zuzhiqxglxx' + [i];
-                    obj.text = zuzhiqxglxx[i].jigoumc;
-                    obj.parent = '#';
-                    fanhui_zuzhiqxglxx.push(obj);
-                    // 部门信息
-                    for(var j in zuzhiqxglxx[i].bumenxx){
-                        var obj = {};
-                        obj.id = zuzhiqxglxx[i].bumenxx[j].bumenbh + 'zuzhiqxglxx' + [i] + 'bumenxx' +[j];
-                        obj.text = zuzhiqxglxx[i].bumenxx[j].bumenmc;
-                        obj.parent = zuzhiqxglxx[i].jigoubh + 'zuzhiqxglxx' + [i];
-                        fanhui_zuzhiqxglxx.push(obj);
-
-                        // 人员信息
-                        for(var k in zuzhiqxglxx[i].bumenxx[j].renyuanxx){
-                            var obj = {};
-                            obj.id = zuzhiqxglxx[i].bumenxx[j].renyuanxx[k].zhanghaobh + [i] + 'bumenxx' +[j] + 'renyuanxx' + [k];
-                            obj.text = zuzhiqxglxx[i].bumenxx[j].renyuanxx[k].xingming;
-                            obj.parent = zuzhiqxglxx[i].bumenxx[j].bumenbh + 'zuzhiqxglxx' + [i] + 'bumenxx' +[j];
-                            fanhui_zuzhiqxglxx.push(obj);
-                        }
-                    }
-                }
-
-                var demo = [
-                    { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" },
-                    { "id" : "ajson2", "parent" : "#", "text" : "Root node 2" },
-                    { "id" : "ajson3", "parent" : "ajson2", "text" : "Child 1" },
-                    { "id" : "ajson4", "parent" : "ajson2", "text" : "Child 2" },
-                ];
-
-                //console.log(fanhui_zuzhiqxglxx);
-                //console.log(demo);
-
-                $("#plugins1").jstree({
-                    "checkbox" : {
-                        "keep_selected_style" : false,
-                        "three_state": false
-                    },
-                    "types" : {
-                        'default' : {
-                            'icon' : 'fa fa-folder'
-                        },
-                    },
-                    /*"contextmenu": {
-                        "items": function ($node) {
-                            var tree = $("#tree").jstree(true);
-                            return {
-                                "Create": {
-                                    "separator_before": false,
-                                    "separator_after": false,
-                                    "label": "新增",
-                                },
-                                "Edit": {
-                                    "separator_before": false,
-                                    "separator_after": false,
-                                    "label": "编辑",
-                                },
-                            };
-                        }
-                    },*/
-
-                    /*"plugins" : [ "checkbox","types", "contextmenu" ],*/
-                    'plugins' : [ 'types', 'dnd' ],
-                    'core' : {
-                        'data': fanhui_zuzhiqxglxx
-                    }
-                });
             });
         }
     })
@@ -221,15 +215,13 @@ Template.zuzhiqxgl.onRendered(function () {
 
 
 Template.zuzhiqxgl.helpers({
-    shuju_zuzhiqxglxx:function () {
-        return Template.instance().zuzhiqxglxx.get();
-    }
+
 });
 
 Template.zuzhiqxgl.events({
     // 二级联动 隐藏显示功能 取消选中功能 新增人员机构 select
     'change #xinzengryjg':function (event) {
-        for(var i =0; i < zuzhiqxglxx.length; i ++){
+        for(var i =0; i < Session.get('zuzhiqxglxx').length; i ++){
             $('.xinzengrybm'+[i]).hide();
         }
         $('#xinzengrybm').val('');
@@ -239,7 +231,7 @@ Template.zuzhiqxgl.events({
     },
     // 二级联动 隐藏显示功能 取消选中功能 编辑部门机构 select
     'change #bianjibmjg':function (event) {
-        for(var i =0; i < zuzhiqxglxx.length; i ++){
+        for(var i =0; i < Session.get('zuzhiqxglxx').length; i ++){
             $('.bianjibmbm'+[i]).hide();
         }
         $('#bianjibmbm').val('');
@@ -249,7 +241,7 @@ Template.zuzhiqxgl.events({
     },
     // 三级联动 隐藏显示功能 取消选中功能 编辑人员机构 select
     'change #bianjiryjg':function (event) {
-        for(var i =0; i < zuzhiqxglxx.length; i ++){
+        for(var i =0; i < Session.get('zuzhiqxglxx').length; i ++){
             $('.bianjirybm'+[i]).hide();
             $('.bianjiryry'+[i]).hide();
         }
@@ -261,7 +253,7 @@ Template.zuzhiqxgl.events({
     },
     // 三级联动 隐藏显示功能 取消选中功能 编辑人员部门 select
     'change #bianjirybm':function (event) {
-        for(var i =0; i < zuzhiqxglxx.length; i ++){
+        for(var i =0; i < Session.get('zuzhiqxglxx').length; i ++){
             $('.bianjiryry'+[i]).hide();
         }
         $('#bianjiryry').val('');
@@ -271,13 +263,13 @@ Template.zuzhiqxgl.events({
     },
     // model-新增-按钮-获取页面数据-向数据库添加数据-关闭模态框
     'click #xinzengjgan':function (event) {
-        var zuzhiqxglxx = {
-            jigoubh: $('#xinzengjgjgbh').val(),
-            jigoumc: $('#xinzengjgjgmc').val()
-        };
-        console.log(zuzhiqxglxx);
+        debugger;
+        var fanhui_zuzhiqxglxx = Session.get('zuzhiqxglxx');
+        fanhui_zuzhiqxglxx.push({jigoubh: $('#xinzengjgjgbh').val(),jigoumc: $('#xinzengjgjgmc').val()});
+        Session.set('zuzhiqxglxx',fanhui_zuzhiqxglxx);
 
-        ts_gc_zuzhijg.insert(zuzhiqxglxx);
+        ts_gc_zuzhijg.insert({jigoubh: $('#xinzengjgjgbh').val(),jigoumc: $('#xinzengjgjgmc').val()});
+        $('input').val('');
         $('#xinzengjgmodel').modal('hide');
     },
     'click #xinzengbman':function (event) {
@@ -305,17 +297,30 @@ Template.zuzhiqxgl.events({
     },
     'click #xinzengryan':function (event) {
         debugger;
-        var renyuanxx = {
-            xingming: $('#xinzengryxm').val(),
-            zhanghaobh: $('#xinzengrybh').val(),
-            zhanghaomc: $('#xinzengrymc').val(),
-            mima: $('#xinzengrymm').val()
-        };
         var id = $('#xinzengryjg').val();
         var bumenmc = $('#xinzengrybm').val();
 
-        //TODO mongodb 更新语句
-        //ts_gc_zuzhijg.insert({_id:id},{'bumenxx':[{'bumenmc':bumenmc}]},{$set:{'bumenxx':[renyuanxx]}});
+        var fanhui_zuzhiqxglxx = zuzhiqxglxx;
+        for(var i in zuzhiqxglxx){
+
+            if(String(zuzhiqxglxx[i]._id).indexOf(id) !=  -1){
+                for(var j in zuzhiqxglxx[i].bumenxx){
+
+                    if(String(zuzhiqxglxx[i].bumenxx[j].bumenmc).indexOf(bumenmc) != -1){
+                        var fanhui_renyuanxx = new Array();
+                        var obj = new Object();
+                        obj.xingming = $('#xinzengryxm').val();
+                        obj.zhanghaobh = $('#xinzengrybh').val();
+                        obj.zhanghaomc = $('#xinzengrymc').val();
+                        obj.mima = $('#xinzengrymm').val();
+                        fanhui_renyuanxx.push(obj);
+                        fanhui_zuzhiqxglxx[i].bumenxx[j].renyuanxx = fanhui_renyuanxx;
+                    }
+                }
+            }
+        }
+
+        ts_gc_zuzhijg.update({_id:id},{$set:[fanhui_zuzhiqxglxx]});
         $('#xinzengrmmodel').modal('hide');
     },
     'click #bianjijgan':function (event) {
